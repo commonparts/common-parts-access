a08532b3cd2f143cbde419363cd2401776dd1345
Arthur Le Dantec <218490320+arthurldtc@users.noreply.github.com>
2025-12-08 22:21:55 +0100

    implemented footer component

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/layout/navbar"
+import { Footer } from "@/components/layout/footer"
 
 export default function PublicLayout({
   children,
@@ -13,11 +14,7 @@ export default function PublicLayout({
           <main>{children}</main>
         </div>
       </div>
-      <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-sm gap-8 py-8 bg-background/50 backdrop-blur-sm">
-        <p>
-          Footer content here. Made with ❤️ by me.
-        </p>
-      </footer>
+      <Footer />
     </div>
   )
 }
