8e966d1244d7967083948b050f10def7342b7854
Arthur Le Dantec <arthur.ledantec@outlook.fr>
2025-12-08 22:21:55 +0100

    implemented footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/layout/navbar";
+import { Footer } from "@/components/layout/footer";
 import { Hero } from "@/components/layout/hero";
 import { FeaturedModels } from "@/components/model/featured-models";
 
@@ -21,11 +22,7 @@ export default function Home() {
             </div>
           </main>
         </div>
-        <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-sm gap-8 py-8 bg-background/50 backdrop-blur-sm">
-          <p>
-            Footer content here. Made with ❤️ by me.
-          </p>
-        </footer>
+        <Footer />
       </div>
     </div>
   );
