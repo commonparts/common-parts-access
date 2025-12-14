7828663c284e5b20f09e5d4b549a1e58ebba2426
Arthur Le Dantec <arthur.ledantec@outlook.fr>
2025-12-14 02:01:37 +0100

    added dashboard layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,9 +1,20 @@
-import type { ReactNode } from "react";
+import Navbar from "@/components/layout/navbar"
+import { Footer } from "@/components/layout/footer"
 
 export default function DashboardLayout({
   children,
 }: {
-  children: ReactNode;
+  children: React.ReactNode
 }) {
-  return <>{children}</>;
+  return (
+    <div className="min-h-screen flex flex-col items-center">
+      <div className="flex-1 w-full flex flex-col items-center">
+        <Navbar />
+        <div className="flex-1 flex flex-col w-full">
+          <main>{children}</main>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  )
 }
