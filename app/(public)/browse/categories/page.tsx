19437ea272e1605bd551c89de2922038d52aa517
Arthur Le Dantec <218490320+arthurldtc@users.noreply.github.com>
2025-12-23 22:02:58 +0100

    removed unused browse pages

--- app/(public)/browse/categories/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function CategoriesPage() {
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Browse by Categories</h1>
-      <p className="text-gray-600">Categories listing will be implemented here</p>
-    </div>
-  )
-}
