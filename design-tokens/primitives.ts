a644d8373a2c5ebc4ca3dcdd0354f2d4a11e8e81
Arthur Le Dantec <218490320+arthurldtc@users.noreply.github.com>
2026-02-16 11:30:47 +0100

    updated spacings tokens

--- design-tokens/primitives.ts
+++ design-tokens/primitives.ts
@@ -41,12 +41,13 @@ export const colorPrimitives = {
 } as const
 
 export const spacingPrimitives = {
-  1: "4px",
-  2: "8px",
-  3: "12px",
-  4: "16px",
-  5: "20px",
-  6: "24px",
+  1: "2px",
+  2: "4px",
+  3: "8px",
+  4: "12px",
+  5: "16px",
+  6: "20px",
+  7: "24px",
   8: "32px",
   10: "40px",
   12: "48px",
